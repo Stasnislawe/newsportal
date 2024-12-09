b47384e80c6074cb803755352c703d87a0338f20
stas_ <dromanqs@mail.ru>
2024-12-09 19:23:13 +0500

    тэг для отображения пользователей из сообщений

--- newsportal/NewsPortal/templatetags/custom_tags.py
+++ newsportal/NewsPortal/templatetags/custom_tags.py
@@ -1,8 +1,16 @@
 from datetime import datetime
 from django import template
-
+from django.contrib.auth.models import User
 
 register = template.Library()
+
+
+@register.simple_tag
+def get_companion(user, chat):
+    for u in chat.members.all():
+        if u != user:
+            return User.objects.get(pk=u.pk)
+    return None
 
 @register.simple_tag()
 def current_time(format_string = '%b %d %Y'):
